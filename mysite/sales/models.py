05afaa0bba3b7b082aa8c7ea819a6281f1c91f3d
ASZabavin <ASZabavin@pnu.edu.ru>
2018-12-21 21:19:07 +1000

    task 10_1 finished
    Signed-off-by: ASZabavin <ASZabavin@pnu.edu.ru>

--- mysite/sales/models.py
+++ mysite/sales/models.py
@@ -1,23 +1,7 @@
 from django.db import models
 
-# TODO Модели добалять сюда.
-# TODO  Необходимо сделать Django модели, которую будут покрывать логику.
-# TODO
-# TODO  Продавцы производят продажи.
-# TODO  Продажи формируются на основе справочника номенклатуры.
-# TODO  Для продаж необходим отслеживать, кем она изменена и когда.
-# TODO  Для продаж формируеются чеки.
-# TODO  Справочники номенлатуры формируется на основе
-# TODO      справочника производителей.
-# TODO      справочника справочника поставщиков.
-# TODO
-# TODO
-# TODO  В докстрингах указать какая предпологается логика, работы с моделями.
-# TODO
-# TODO  Необходимо сформировать миграцию.
+# -- справочник
 
-
-#-- справочник
 
 class DirectoryContractor(models.Model):
     name = models.CharField(u"Наименование контрагента", null=False, blank=False, max_length=400)
@@ -33,7 +17,7 @@ class DirectoryContractor(models.Model):
 
 
 class DirectoryProducer(models.Model):
-    contractor = models.OneToOneField(DirectoryContractor, verbose_name=u"Контрагент", related_name="producer", on_delete="SET NULL")
+    contractor = models.OneToOneField('DirectoryContractor', verbose_name=u"Контрагент", related_name="producer", on_delete="SET NULL")
 
     class Meta():
         verbose_name = u"производитель"
@@ -45,7 +29,7 @@ class DirectoryProducer(models.Model):
 
 
 class DirectoryProvider(models.Model):
-    contractor = models.OneToOneField(DirectoryContractor, verbose_name=u"Контрагент", related_name="provider", on_delete="SET NULL")
+    contractor = models.OneToOneField('DirectoryContractor', verbose_name=u"Контрагент", related_name="provider", on_delete="SET NULL")
 
     class Meta():
         verbose_name = u"поставщик"
@@ -58,9 +42,10 @@ class DirectoryProvider(models.Model):
 
 class DirectoryNomenclature(models.Model):
     name = models.CharField(u"Наименование товара", null=False, blank=False, max_length=400)
+    price = models.DecimalField(u"Цена товара", null=False, blank=False, max_digits=12, decimal_places=2)
 
-    producers = models.ManyToManyField(DirectoryProducer, verbose_name=u"Производители", related_name="nomenclatures", )
-    providers = models.ManyToManyField(DirectoryProvider, verbose_name=u"Поставщики", related_name="nomenclatures", )
+    producers = models.ManyToManyField('DirectoryProducer', verbose_name=u"Производители", related_name="nomenclatures", )
+    providers = models.ManyToManyField('DirectoryProvider', verbose_name=u"Поставщики", related_name="nomenclatures", )
 
     class Meta():
         verbose_name = u"номенклатура"
@@ -76,11 +61,11 @@ class DirectoryNomenclature(models.Model):
 
 
 class SalesUser(models.Model):
-    name = models.CharField(u"Имя юзера", null=False, blank=False, max_length=400)
+    name = models.CharField(u"Имя покупателя", null=False, blank=False, max_length=400)
 
     class Meta():
-        verbose_name = u"юзер приложения"
-        verbose_name_plural = u"юзеры приложения"
+        verbose_name = u"покупатель"
+        verbose_name_plural = u"покупатели"
         app_label = "sales"
         ordering = ['name']
 
@@ -88,45 +73,40 @@ class SalesUser(models.Model):
         return self.name
 
 
-class UpdatedFieldsMixin(models.Model):
-    updated_by = models.ForeignKey(SalesUser, verbose_name=u"Юзер, который обновил", on_delete="SET NULL")
-    updated = models.DateTimeField(blank=True, null=True, auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Seller(models.Model):
-    user = models.OneToOneField(DirectoryContractor, verbose_name=u"Юзер", related_name="seller", on_delete="SET NULL")
-
-    class Meta():
-        verbose_name = u"продавец"
-        verbose_name_plural = u"продавцы"
-        app_label = "sales"
-
-    def __str__(self):
-        return 'Продавец: {}'.format(self.user)
-
-
-class Sale(UpdatedFieldsMixin, models.Model):
-    nomenclatures = models.ManyToManyField(DirectoryNomenclature, verbose_name=u"Номенклатуры товаров", related_name="sales", )
+class Sale(models.Model):
+    nomenclature = models.ForeignKey(DirectoryNomenclature, verbose_name=u"Номенклатура товаров", related_name="sales", on_delete="SET NULL")
+    number = models.DecimalField(u"Количество закупки", null=False, blank=False, max_digits=12, decimal_places=2)
 
     class Meta():
         verbose_name = u"продажа (транзакция)"
         verbose_name_plural = u"продажи"
         app_label = "sales"
 
+    @property
+    def summ(self):
+        return self.nomenclature.price * self.number
+
     def __str__(self):
-        return 'Товары: {}'.format(', '.join(map(str, self.nomenclatures.all())))
+        return 'Продажа: {}, кол. {:,}, сумма {:,}'.format(self.nomenclature, self.number, self.summ)
 
 
-class Check(models.Model):
-    sale = models.OneToOneField(Sale, verbose_name=u"Набор товаров (транзакция)", related_name="seller", on_delete="SET NULL")
+class SaleCheck(models.Model):
+
+    sell_to = models.ForeignKey('SalesUser', verbose_name=u"Покупатель", on_delete="SET NULL")
+    selled = models.DateTimeField(verbose_name=u"Когда продано", blank=True, null=True, auto_now_add=True)
+
+    sales = models.ManyToManyField('Sale', verbose_name=u"Закупка", related_name="sale_check")
+    summ = models.DecimalField(u"Сумма закупки", null=False, default='0', blank=True, max_digits=12, decimal_places=2)
 
     class Meta():
         verbose_name = u"чек"
         verbose_name_plural = u"чеки"
         app_label = "sales"
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.summ = sum([x.summ for x in self.sales.all()])
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return 'Чек на: [{}]'.format(self.sale)
+        return 'Чек на: [{}]'.format(', '.join(map(str, self.sales.all())))[:100]
