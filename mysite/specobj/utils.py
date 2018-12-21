c3d41f2d857af80776ac8cc0f18e1ecc9af21fc3
ASZabavin <ASZabavin@pnu.edu.ru>
2018-12-21 22:31:08 +1000

    task 10_3 finished?

--- mysite/specobj/utils.py
+++ mysite/specobj/utils.py
@@ -1,9 +1,9 @@
 # TODO Для существующей моделей, необходимо написать функции
 # TODO (объект и характеристика, это модели в аппке):
 # TODO
-# TODO Принимает объект и возвращает все его актуальные характеристики.
-# TODO Возвращает queryset c актуальными спеками на текущую дату.
-# TODO Возвращает queryset объектов с актульными спеками.
+# TODO Принимает объект и возвращает все его актуальные характеристики.+
+# TODO Возвращает queryset c актуальными спеками на текущую дату.+
+# TODO Возвращает queryset объектов с актульными спеками.+
 # TODO Принимает объект и помечает спеки объектов как не актульные, для даты меньше указаной.
 # TODO Помечать на удаление объекты у которых нет актуальных спек.
 # TODO Удалять объекты которые помечены на удаление.
@@ -14,18 +14,22 @@ import datetime
 from .models import Obj, Spec
 
 
-def get_actual_characteristics(obj):
-    characteristics = Spec.objects.filter(link=obj).all()
-    return characteristics
-
-
-def get_actual_characteristics_today():
+def get_actual_specs(obj):
     today = datetime.date.today()
-    actual_chstics = Spec.objects.filter(date=today)
-    return actual_chstics
+    specs = Spec.objects.filter(link=obj).filter(date=today)
+    return specs
 
 
-def get_actual_objects_today():
-    actual_chstics = get_actual_characteristics_today()
-    actual_objs = actual_chstics.select_related('link')
-    return actual_objs
+def set_not_actual_specs(obj):
+    today = datetime.date.today()
+    updated = Spec.objects.filter(link=obj).filter(date__lt=today).update(is_actual=False)
+
+
+def set_delobj_not_actual_specs():
+    today = datetime.date.today()
+    updated = Spec.objects.filter(date__lt=today).select_related('link').update(link__is_deleted=True)
+
+
+def del_is_delete_obj():
+    today = datetime.date.today()
+    deleted = Obj.objects.filter(is_deleted=True).delete()
